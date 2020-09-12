62dccda5796f34aef7e8ae2cb004398daf6d3907
Vynter <amine-cheraitia@hotmail.com>
2020-09-12 22:46:39 +0200

    2nd

--- js/index.js
+++ js/index.js
@@ -7,6 +7,30 @@ const clear = document.getElementById("clear");
 const url = document.getElementById("url");
 const load = document.getElementById("load");
 
-console.log(url);
-const tasks = [];
-//video 35
+const tasks = ["task1", "task2"];
+
+function taskToDom(task) {
+  if (typeof task === "string" && task) {
+    let newItem = document.createElement("li");
+    let rmv = document.createElement("button");
+
+    newItem.textContent = task;
+    rmv.textContent = "REMOVE";
+
+    newItem.appendChild(rmv);
+
+    list.insertBefore(newItem, list.firstChild);
+  }
+}
+
+for (let i = 0; i < tasks.length; i++) {
+  taskToDom(tasks[i]);
+}
+
+add.addEventListener("click", function () {
+  let c = input.value;
+  let newOne = document.createElement("li");
+  newOne.innerText = c;
+  list.appendChild(newOne);
+  console.log(newOne);
+});
