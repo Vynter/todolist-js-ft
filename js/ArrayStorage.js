d728a0da6cd9ee264823b90b7c7ab0a0330c9607
Vynter <amine-cheraitia@hotmail.com>
2020-09-14 12:51:52 +0200

    last commit

--- js/ArrayStorage.js
+++ js/ArrayStorage.js
@@ -1,1 +1,31 @@
+"use strict";
 //stockage sur le navigateurs
+class ArrayStorage {
+  //keyname and value
+  constructor(name) {
+    this.name = name;
+    this.list = this.get();
+  }
+  //récuéprer tableau de valeur
+  get() {
+    if (!localStorage.getItem(this.name)) {
+      localStorage.setItem(this.name, "[]");
+    }
+    return JSON.parse(localStorage.getItem(this.name));
+  }
+  //set a new value in the array
+  set(value) {
+    this.list.push(value);
+    localStorage.setItem(this.name, JSON.stringify(this.list));
+  }
+  //delete a value from the array
+  remove(value) {
+    const index = this.list.indexOf(value);
+    this.list.splice(index, 1);
+    localStorage.setItem(this.name, JSON.stringify(this.list));
+  }
+  //vider le tableau
+  clear() {
+    localStorage.removeItem(this.name);
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // app
 //
+"use strict";
 const list = document.getElementById("list");
 const input = document.getElementById("input");
 const add = document.getElementById("add");
@@ -7,7 +8,9 @@ const clear = document.getElementById("clear");
 const url = document.getElementById("url");
 const load = document.getElementById("load");
 
-const tasks = ["task1", "task2"];
+//new instance w key (tasks)
+const storage = new ArrayStorage("tasks");
+const tasks = storage.list;
 
 function taskToDom(task) {
   if (typeof task === "string" && task) {
@@ -18,13 +21,17 @@ function taskToDom(task) {
     rmv.textContent = "REMOVE";
 
     rmv.addEventListener("click", () => {
+      const value = rmv.parentNode.firstChild.textContent;
+      storage.remove(value);
       list.removeChild(rmv.parentNode);
     });
 
     newLi.appendChild(rmv);
 
     list.insertBefore(newLi, list.firstChild);
+    return true;
   }
+  return false;
 }
 
 tasks.forEach((task) => {
@@ -36,6 +43,10 @@ tasks.forEach((task) => {
 // }
 //used in click event and pressing enter to add data
 function newTask() {
+  if (storage.list.indexOf(input.value) === -1 && taskToDom(input.value)) {
+    storage.set(input.value);
+    input.value = "";
+  }
   input.focus(); //garder curseur
 }
 
@@ -45,8 +56,10 @@ input.addEventListener("keydown", (e) => {
   }
 });
 
-add.addEventListener("click", function () {
-  let c = input.value;
+add.addEventListener("click", newTask);
+//{
+//newTask();
+/*let c = input.value;
   let newOne = document.createElement("li");
   let rmv = document.createElement("button");
 
@@ -57,13 +70,36 @@ add.addEventListener("click", function () {
   });
   newOne.appendChild(rmv);
   list.insertBefore(newOne, list.firstChild);
-});
+  input.value = "";*/
+//});
 
 //clear
 clear.addEventListener("click", () => {
+  storage.clear();
   list.innerHTML = "";
 });
 
 //load of the tasks from a json file
-load.addEventListener("click", () => {});
+load.addEventListener("click", () => {
+  fetch(url.value)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(
+        `${response.statusText} avec le code ${response.status} `
+      );
+    })
+    .then((tasks) => {
+      if (Array.isArray(tasks)) {
+        tasks.forEach((task) => {
+          if (storage.list.indexOf(task) === -1 && taskToDom(task)) {
+            storage.set(task);
+          }
+        });
+        return;
+      }
+      throw new TypeError(`la réponse n'est pas un tableau  )`);
+    });
+});
 //63
